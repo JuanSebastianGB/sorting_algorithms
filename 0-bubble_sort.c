4a96b28842613c0a55fd75742cc2ba9991914f95
JuanSebastianGB <3393@holbertonschool.com>
2021-10-20 14:56:37 +0000

    UPL: Uploading files to implement task 0, sorting bubble algorytm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order using
+ *  the Bubble sort algorithm
+ * @array: array to sort
+ * @size: length of the array
+ * Return: Nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	(void)array;
+	(void)size;
+	printf("something");
+}
