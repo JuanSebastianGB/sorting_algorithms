196d204d22b1f0ba04e8bdf6817b699b0ff9e33d
JuanSebastianGB <3393@holbertonschool.com>
2021-10-20 17:11:28 +0000

    UPL: Uploading files for implementing insertion sort algorythm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ *  in ascending order using the Insertion sort algorithm
+ * @list: list to look into
+ * Return: Nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+	print("");
+}
