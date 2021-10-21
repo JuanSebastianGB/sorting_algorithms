4f11a313c29ac2cd18d6f608dc61b77ff1cddda3
JuanSebastianGB <3393@holbertonschool.com>
2021-10-21 22:15:09 +0000

    UPD: Making some tests to work with a swap function to implement the insert algorythm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,24 @@
 #include "sort.h"
 
 /**
+ * make_swap - swap 2 nodes left and right and make the runner from right
+ * to left
+ * @prev: Prev node
+ * @actual: Actual node
+ * @head: Head node
+ * Return Nothing
+ */
+
+void make_swap(listint_t **prev, listint_t *actual, listint_t **head)
+{
+	(void)actual;
+	(void)prev;
+	printf("(Prev: %i)", (*prev)->n);
+	printf("(Actual: %i)\n", actual->n);
+	printf("(Cabecera: %i)\n", (*head)->n);
+}
+
+/**
  * insertion_sort_list - sorts a doubly linked list of integers
  *  in ascending order using the Insertion sort algorithm
  * @list: list to look into
@@ -8,5 +26,11 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	print("");
+	listint_t *actual;
+
+	for (actual = (*list)->next; actual != NULL; actual = actual->next)
+	{
+		if (actual->prev != NULL && actual->n < actual->prev->n)
+			make_swap(&actual->prev, actual, list);
+	}
 }
