895d58e33bd1aeef4e817a58b09fe03646748908
JuanSebastianGB <3393@holbertonschool.com>
2021-10-22 02:46:13 +0000

    UPD: Adding validation for NULL inputs

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -40,6 +40,9 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *actual, *prev, *tmp;
 
+	if (list == NULL || *list == NULL)
+		return;
+
 	for (actual = (*list)->next; actual != NULL; actual = tmp)
 	{
 		prev = actual->prev;
