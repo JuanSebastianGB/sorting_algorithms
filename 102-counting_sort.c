a0b7651852ea7b8353dcb786f93824caa7900cd7
JuanSebastianGB <3393@holbertonschool.com>
2021-10-24 04:02:01 +0000

    UPL: Uploading files needed to implement function for counting sort

---
+++ 102-counting_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * counting_sort - sorts an array of integers in ascending order
+ * using the Counting sort algorithm
+ * @array: Array to sort
+ * @size: Size of the array
+ */
+void counting_sort(int *array, size_t size)
+{
+	print_array(array, size);
+}
