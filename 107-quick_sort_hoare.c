507dbc67679d4732d0e6afc711e78721b5fd5cef
JuanSebastianGB <3393@holbertonschool.com>
2021-10-27 04:54:26 +0000

    UPL: Uploading the required files to implement quick sort hoare algorithm, including the prototype

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * quick_sort_hoare - sorts an array of integers
+ *  in ascending order using the Quick sort algorithm
+ * @array: Array to sort
+ * @size: Size of the array
+ * Return: Nothing
+ *
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	print_array(array, size);
+}
