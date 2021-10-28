3400be99b82e20d9ff83849110df61bb99fb1e04
JuanSebastianGB <3393@holbertonschool.com>
2021-10-28 04:55:19 +0000

    UPL: Uploading required files to start the implementation of the sorting deck task, last advance

---
+++ 1000-sort_deck.c
@@ -0,0 +1,12 @@
+#include "deck.h"
+
+/**
+ * sort_deck - sorts a deck of cards.
+ * @deck: Deck to sort
+ * Return: Nothing
+ *
+ */
+void sort_deck(deck_node_t **deck)
+{
+	printf("Testing [%s]", (*deck)->card->value);
+}
