cac126d197a798f4726c209c343fd902b7b40546
JuanSebastianGB <3393@holbertonschool.com>
2021-10-31 01:01:48 +0000

    UPD: Creating a function to get the numerical value of a gived card

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,6 +1,26 @@
 #include "deck.h"
 
 /**
+ * get_card_value - Getting the int card value to work with
+ * @card: Actual card to get value
+ * Return: Card's Value
+ *
+ */
+int get_card_value(deck_node_t *card)
+{
+	int value = atoi(card->card->value);
+
+	if (value != 0)
+		return (value);
+	if (strcmp(card->card->value, "Jack") == 0)
+		return (11);
+	if (strcmp(card->card->value, "Queen") == 0)
+		return (12);
+	if (strcmp(card->card->value, "King") == 0)
+		return (13);
+	return (0);
+}
+/**
  * sort_deck - sorts a deck of cards.
  * @deck: Deck to sort
  * Return: Nothing
@@ -10,4 +30,9 @@ void sort_deck(deck_node_t **deck)
 {
 	if (deck == NULL || *deck == NULL)
 		return;
+	while (*deck)
+	{
+		printf("El valor de la carta actual es: %d\n", get_card_value(*deck));
+		*deck = (*deck)->next;
+	}
 }
--- deck.h
+++ deck.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef enum kind_e
 {
