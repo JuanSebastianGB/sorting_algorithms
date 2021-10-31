76031496ac9b572a03ef01d7eff33828d2ba2aeb
JuanSebastianGB <3393@holbertonschool.com>
2021-10-31 03:16:51 +0000

    UPD: Deleting unnecesary function

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,6 +1,26 @@
 #include "deck.h"
 
 /**
+ * *_strcmp - compare two strings
+ *@s1: pointer 1
+ *@s2: pointer 2
+ *Return: An integer + - or 0
+ */
+
+int _strcmp(const char *s1, const char *s2)
+{
+	int i = 0;
+
+	while (*(s1 + i) && *(s2 + i))
+	{
+		if (s1[i] != s2[i])
+			break;
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+/**
  * get_card_value - Getting the int card value to work with
  * @card: Actual card to get value
  * Return: Card's Value
@@ -13,11 +33,11 @@ int get_card_value(deck_node_t *card)
 
 	if (value != 0)
 		return (value);
-	if (strcmp(card->card->value, "Jack") == 0)
+	if (_strcmp(card->card->value, "Jack") == 0)
 		return (11);
-	if (strcmp(card->card->value, "Queen") == 0)
+	if (_strcmp(card->card->value, "Queen") == 0)
 		return (12);
-	if (strcmp(card->card->value, "King") == 0)
+	if (_strcmp(card->card->value, "King") == 0)
 		return (13);
 	return (0);
 }
@@ -47,27 +67,6 @@ void swap_nodes(deck_node_t **prev, deck_node_t *actual,
 }
 
 /**
- * implement_going_back - Implements going back for sorting algorythm
- * to left
- * @prev: Prev node
- * @actual: Actual node
- * @head: Head node to print list
- * @option: sort by value if 0, otherwise sort by kind
- * Return Nothing
- */
-
-void implement_going_back(deck_node_t **prev, deck_node_t *actual,
-						  deck_node_t **head, int option)
-{
-	if (!option)
-		while (actual->prev != NULL && get_card_value(actual) < get_card_value(actual->prev))
-			swap_nodes(prev, actual, head);
-	else
-		while (actual->prev != NULL && actual->card->kind < actual->prev->card->kind)
-			swap_nodes(prev, actual, head);
-}
-
-/**
  * insertion_sort_list - sorts a deck of cards
  *  in ascending order using the Insertion sort algorithm
  * @list: list to look into
@@ -84,7 +83,12 @@ void insertion_sort_list(deck_node_t **list, int option)
 	for (actual = (*list)->next; actual != NULL; actual = actual->next)
 	{
 		prev = actual->prev;
-		implement_going_back(&prev, actual, list, option);
+		if (!option)
+			while (actual->prev != NULL && get_card_value(actual) < get_card_value(actual->prev))
+				swap_nodes(&prev, actual, list);
+		else
+			while (actual->prev != NULL && actual->card->kind < actual->prev->card->kind)
+				swap_nodes(&prev, actual, list);
 	}
 }
 
